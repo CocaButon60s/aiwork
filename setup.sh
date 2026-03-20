5785c1dfa50696f565ec672a6a04592ee22f3834
Hiroyuki Ikuno <sam2kaikaramegusuri@gmail.com>
2026-03-20 19:16:07 +0900

    update

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@
 [ ! -d "ComfyUI" ] && git clone https://github.com/comfyanonymous/ComfyUI.git
 (
     cd ComfyUI/custom_nodes
-    [ ! -d comfyui-manager ] && git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
+    [ ! -d "comfyui-manager" ] && git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
 )
