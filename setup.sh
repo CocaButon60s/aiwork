d87abf5bfa74e1e820b305bd3c5f4dce24f5af6b
Hiroyuki Ikuno <sam2kaikaramegusuri@gmail.com>
2026-03-20 16:34:05 +0900

    update

--- setup.sh
+++ setup.sh
@@ -4,10 +4,6 @@ git submodule update --init --recursive
 (
     cd ComfyUI/custom_nodes
     if [ ! -d comfyui-manager ]; then
-        git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager --depth 1
-    fi
-    # resize image and remove bg
-    if [ ! -d comfyui_essentials ]; then
-        git clone https://github.com/cubiq/ComfyUI_essentials.git comfyui_essentials --depth 1
+        git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
     fi
 )
