fd8b5d9253bf85f42775ae246126bb7f7db93c47
Hiroyuki Ikuno <sam2kaikaramegusuri@gmail.com>
2026-03-20 19:13:00 +0900

    update

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-git submodule update --init --recursive
+[ ! -d "sd-scripts" ] && git clone https://github.com/kohya-ss/sd-scripts.git
+[ ! -d "ComfyUI" ] && git clone https://github.com/comfyanonymous/ComfyUI.git
 (
     cd ComfyUI/custom_nodes
-    if [ ! -d comfyui-manager ]; then
-        git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
-    fi
+    [ ! -d comfyui-manager ] && git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
 )
